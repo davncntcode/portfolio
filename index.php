f8a715be120a5c1122c2bae7601ef010683b7fd8
davincentcode <vincentisraelprias@gmail.com>
2023-06-20 21:24:43 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/loader.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	<title>Da Vincent Code</title>
+</head>
+<!-- <div id="full-screen">
+		<div class="banter-loader">
+		  <div class="banter-loader__box"></div>
+		  <div class="banter-loader__box"></div>
+		  <div class="banter-loader__box"></div>
+		  <div class="banter-loader__box"></div>
+		  <div class="banter-loader__box"></div>
+		  <div class="banter-loader__box"></div>
+		  <div class="banter-loader__box"></div>
+		  <div class="banter-loader__box"></div>
+		  <div class="banter-loader__box"></div>
+		</div>
+	</div>
+<body> -->
+
+	<div id="content-body" class="container">
+		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		  <div class="container-fluid justify-content-center">
+		    <a class="navbar-brand" href="#">davncnt.<span class="nav-red">code</spam></a>
+		  </div>
+		</nav>
+
+		<div class="sidebar">
+		  <a class="active" href="#home"><i class="fa fa-home" aria-hidden="true"></i><span class="menutext">&nbsp;Home</span></a>
+		  <a href="#education"><i class="fa fa-graduation-cap" aria-hidden="true"></i><span class="menutext">&nbsp;Education</span></a>
+		  <a href="#experience"><i class="fa fa-bars" aria-hidden="true"></i><span class="menutext">&nbsp;Experience</span></a>
+		  <a href="#projectsdone"><i class="fa fa-rocket" aria-hidden="true"></i><span class="menutext">&nbsp;Projects</span></a>
+		  <a href="#contact"><i class="fa fa-user" aria-hidden="true"></i><span class="menutext">&nbsp;Contact</span></a>
+		</div>
+	</div>
+
+</body>
+</html>
