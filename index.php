18ac82a6e08a3e89f519825820a1d85f99b5cc0b
davincentcode <vincentisraelprias@gmail.com>
2023-06-25 00:34:28 +0800

    about me and skills

--- index.php
+++ index.php
@@ -4,18 +4,33 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Da Vincent Code</title>
-	<?php require 'views/assets.php';?>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/loader.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
 
-	<div class="home-container">
-		<div class="home-form">
-			<h4>First, how may I call you?</h4>
-			<input id="name-inp" type="text" name="" placeholder="Please enter your name" autocomplete="off" class="shadow-none rounded-0 form-control mb-3">
-				<button type="button" class="rounded-0 mr-3 btn btn-primary">Submit</button>
-				<button type="button" class="rounded-0 btn" style="color: whitesmoke;">Skip</button>
+	<div class="intro-container">
+		<div class="intro-form">
+			<form method="post" action="views/home.php">
+				<h4>First, how may I call you?</h4>
+				<input id="name-inp" name="name-inp" type="text" name="" placeholder="Please enter your name" autocomplete="off" class="shadow-none rounded-0 form-control mb-3">
+					<button id="btnsub-name" type="submit" class="rounded-1 mr-5 btn btn-primary" style="margin-right: 20px;">Submit</button>
+					<a id="skip" class="" style="color: whitesmoke;" href="views/home.php">Skip</a>
+			</form>
 		</div>
 	</div>
 
+
 </body>
 </html>
+
+<script type="text/javascript">
+	
+</script>
--- views/home.php
+++ views/home.php
@@ -3,47 +3,138 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/loader.css">
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-	<title>Da Vincent Code</title>
+	<title>Da Vincent Code | Home</title>
+	<?php require 'assets.php';?>
 </head>
-<!-- <div id="full-screen">
-		<div class="banter-loader">
-		  <div class="banter-loader__box"></div>
-		  <div class="banter-loader__box"></div>
-		  <div class="banter-loader__box"></div>
-		  <div class="banter-loader__box"></div>
-		  <div class="banter-loader__box"></div>
-		  <div class="banter-loader__box"></div>
-		  <div class="banter-loader__box"></div>
-		  <div class="banter-loader__box"></div>
-		  <div class="banter-loader__box"></div>
-		</div>
-	</div>-->
+	<!-- <div id="full-screen">
+			<div class="banter-loader">
+			  <div class="banter-loader__box"></div>
+			  <div class="banter-loader__box"></div>
+			  <div class="banter-loader__box"></div>
+			  <div class="banter-loader__box"></div>
+			  <div class="banter-loader__box"></div>
+			  <div class="banter-loader__box"></div>
+			  <div class="banter-loader__box"></div>
+			  <div class="banter-loader__box"></div>
+			  <div class="banter-loader__box"></div>
+			</div>
+	</div> -->
 <body> 
 
 	<div id="content-body" class="container">
-		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-		  <div class="container-fluid justify-content-center">
-		    <a class="navbar-brand" href="#">davncnt.<span class="nav-red">code</spam></a>
-		  </div>
-		</nav>
+		<div class="home-container">
+			<div class="hello">
+				<?php
+				    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				      $name = $_POST["name-inp"];
+				      echo '<h1 id="user-name" style="color:whitesmoke;">Hello, ' . $name . '!</h1>';
+				    }
+				    else {
+				      echo '<h1 id="user-name" style="color:whitesmoke;">Hello!</h1>';
+				    }
+				 ?>
+			</div>
+			<div class="home-message">
+				I am <span class="main-text-color">Vincent Israel Prias</span>. I develop systems and websites.
+			</div>
+			<div class="col-md-12 px-5">
+			  <div class="ml-auto mr-auto justify-content-around home-nav row">
+			  	<div class="col-md-2 text-center"><a class="">ABOUT</a></div>
+			    <!-- <div class="col-md-2 text-center"><a class="">EDUCATION</a></div> -->
+			    <div class="col-md-2 text-center"><a class="">SKILLS</a></div>
+			    <div class="col-md-2 text-center"><a class="">EXPERIENCE</a></div>
+			    <div class="col-md-2 text-center"><a class="">CONTACT</a></div>
+			  </div>
+			</div>
+		</div>
 
-		<div class="sidebar">
+
+		<div class="about-container pt-3">
+		  <div class="row w-100">
+		    <div class="col-md-6 text-center col-sm-12 p-3">
+		    	<div class="d-flex flex-column">
+			        <h6 id="about-title">ABOUT</h6>
+			        <h1 id="about-title">ME</h1>
+			    </div>
+		    </div>
+		    <div class="col-md-6 col-sm-12 p-3 d-flex align-items-center">
+			  <div class="d-flex flex-column">
+			    <span class="d-block">System Developer;</span>
+			    <span class="d-block">Web Developer;</span>
+			    <span class="d-block">Tech Enthusiast;</span>
+
+			    <div class="about-desc mt-3">
+			  	<p>
+			  		"Experienced tech enthusiast with a passion for innovation, collaboration, and continuous improvement. Skilled in software development, system administration, and effective communication. Valuable asset for successful project outcomes."
+			  	</p>
+			  </div>
+			  </div>
+			  
+			</div>
+		  </div>
+		</div>
+
+		<div class="skills-container">
+			<h1>SKILLS</h1>
+			<div class="carousel-container">
+			   <section class="customer-logos slider">
+			      <div class="slide d-flex align-items-center justify-content-center">
+			      	<div class="text-center">
+				      	<img class="logo-img" src="../assets/images/html5.png">
+				      	<!-- <h3 style="color:whitesmoke;">Advance</h3> -->
+			      	</div>
+			      </div>
+			      <div class="slide d-flex align-items-center justify-content-center"><img class="logo-img" src="../assets/images/css3.png"></div>
+			      <div class="slide d-flex align-items-center justify-content-center"><img class="logo-img" src="../assets/images/php.png"></div>
+			       <div class="slide d-flex align-items-center justify-content-center"><img class="logo-img" src="../assets/images/js.png"></div>
+			       <div class="slide d-flex align-items-center justify-content-center"><img class="logo-img" src="../assets/images/bs.png"></div>
+			       <div class="slide d-flex align-items-center justify-content-center"><img class="logo-img" src="../assets/images/csharp.png"></div>
+			       <div class="slide d-flex align-items-center justify-content-center"><img class="logo-img" src="../assets/images/react.png"></div>
+			       <div class="slide d-flex align-items-center justify-content-center"><img class="logo-img" src="../assets/images/node.png"></div>
+			       <div class="slide d-flex align-items-center justify-content-center"><img class="logo-img" src="../assets/images/sql.png"></div>
+			   </section>
+			</div>
+		</div>	
+
+		<!-- <div class="sidebar">
 		  <a class="active" href="#home"><i class="fa fa-home" aria-hidden="true"></i><span class="menutext">&nbsp;Home</span></a>
 		  <a href="#education"><i class="fa fa-graduation-cap" aria-hidden="true"></i><span class="menutext">&nbsp;Education</span></a>
 		  <a href="#experience"><i class="fa fa-bars" aria-hidden="true"></i><span class="menutext">&nbsp;Experience</span></a>
 		  <a href="#projectsdone"><i class="fa fa-rocket" aria-hidden="true"></i><span class="menutext">&nbsp;Projects</span></a>
 		  <a href="#contact"><i class="fa fa-user" aria-hidden="true"></i><span class="menutext">&nbsp;Contact</span></a>
-		</div>
+		</div> -->
 	</div>
 
 </body>
 </html>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+    // Show the div
+    //$('#full-screen').fadeIn();
+
+    // Delay for 3 seconds
+    //$('#full-screen').delay(2000).fadeOut();
+		$('.customer-logos').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+  });
+
+</script>
